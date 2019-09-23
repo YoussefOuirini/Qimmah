b8cbcde85f5ec689701cfae8ac489cf62070ca34
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-09-23 15:50:31 +0200

    adjusting rendering

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import RegistrationForm from "./RegistrationForm/RegistrationForm";
+import RegistrationForm from "./RegistrationForm/RegistrationForm.vue";
 
 new Vue({
   render: h => h(RegistrationForm)
