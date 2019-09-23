46d9552f367de5244a46ea80483a235c99d2e06e
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-09-23 15:02:36 +0200

    adding basic form

--- main.js
+++ main.js
@@ -2,8 +2,35 @@ const Vue = require('vue/dist/vue.esm').default;
 
 Vue.component('registration-form', {
   template: `
-    <h1>Hello, World</h1>
+    <div>
+      <div>
+        <h2>First Name</h2>
+        <input type="text" v-model="firstName" />
+      </div>
+
+      <div>
+        <h2>Last Name</h2>
+        <input type="text" v-model="lastName" />
+      </div>
+
+      <div>
+        <h2>Submit</h2>
+        <input type="submit" value="Submit" v-on:click="submit" />
+      </div>
+    </div>
   `,
+  data: () => ({
+    firstName: '',
+    lastName: ''
+  }),
+  methods: {
+    submit: function() {
+      console.log(this.firstName, this.lastName);
+
+      this.firstName = '';
+      this.lastName = '';
+    }
+  }
 });
 
 new Vue({ el: '#content' });
