09b645b2143eca5de652150a6eb6d0cff69e3cb8
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-09-24 13:01:19 +0200

    Adding Footer component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Header />
     <RegistrationForm />
+    <Footer />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
   import Vue from "vue";
   import RegistrationForm from "./RegistrationForm/RegistrationForm.vue";
   import Header from "./Header/Header.vue"
+  import Footer from "./Footer/Footer.vue"
 
   export default Vue.extend({
     name: "App",
     components: {
       Header,
-      RegistrationForm
+      RegistrationForm,
+      Footer
     }
   })
 </script>
