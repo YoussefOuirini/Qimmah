33720504abac9dd0df474f930baf7d50c0e129c5
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-09-24 14:32:18 +0200

    Adding logic to check age of consent

--- src/RegistrationForm/RegistrationForm.js
+++ src/RegistrationForm/RegistrationForm.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import { config } from "../config";
 
 export default Vue.extend({
   name: "RegistrationForm",
@@ -8,6 +9,7 @@ export default Vue.extend({
       lastName: '',
       gender: '',
       birthDate: '',
+      underage: false,
       adress: '',
       zipCode: '',
       city: '',
@@ -17,6 +19,21 @@ export default Vue.extend({
     }
   },
   methods: {
+    checkAge() {
+      const birthDateTimeStamp = new Date(this.birthDate).getTime();
+      const ageLimitTimeStamp = this.getAgeLimitTimeStamp()
+      if (birthDateTimeStamp > ageLimitTimeStamp) {
+        this.underage = true
+      } else {
+        this.underage = false
+      }
+    },
+    getAgeLimitTimeStamp() {
+      const date = new Date();
+      date.setFullYear(date.getFullYear() - config.ageOfConsent)
+      const ageLimitTimeStamp = date.getTime();
+      return ageLimitTimeStamp
+    },
     submit() {
       alert(`U bent ingeschreven met de volgende informatie: ${this.firstName} ${this.lastName}.`);
     }
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const config = {
+  ageOfConsent: 16
+};
