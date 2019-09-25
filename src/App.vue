0bad959c7793a88922035880eb4ab21b31af0ec2
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-09-25 16:36:13 +0200

    Readding styling background

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 <script>
   import Vue from "vue";
   import RegistrationForm from "./components/RegistrationForm/RegistrationForm.vue";
-  import Header from "./components/Header/Header.vue"
-  import Footer from "./components/Footer/Footer.vue"
+  import Header from "./components/Header/Header.vue";
+  import Footer from "./components/Footer/Footer.vue";
 
   export default Vue.extend({
     name: "App",
