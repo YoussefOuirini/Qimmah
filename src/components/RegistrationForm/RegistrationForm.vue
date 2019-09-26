5aa21cd3b626635c17cf0e2e639b6664579b4c03
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-09-26 10:23:19 +0200

    creating neste adres form

--- src/components/RegistrationForm/RegistrationForm.vue
+++ src/components/RegistrationForm/RegistrationForm.vue
@@ -1,18 +1,22 @@
 <template>
   <b-container>
+    <h1>Inschrijfformulier Dar al-Qoraan Al-Himmah</h1>
     <b-form @submit="submit">
-      <b-form-group id="input-group-1" label="Vul uw naam in" label-for="input-2">
+      <b-form-group id="input-group-1" label="Vul uw voornaam in" label-for="input-1">
         <b-form-input
           id="input-1"
           v-model="firstName"
           required
-          placeholder="Vul uw voornaam in"
+          placeholder="Voornaam"
         ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Vul uw achternaam in" label-for="input-2">
         <b-form-input
           id="input-2"
           v-model="lastName"
           required
-          placeholder="Vul uw achternaam in"
+          placeholder="Achternaam"
         ></b-form-input>
       </b-form-group>
 
@@ -27,9 +31,9 @@
       </div> -->
 
       <b-form-group
-        id="input-group-2"
+        id="input-group-3"
         label="Email adres:"
-        label-for="input-2"
+        label-for="input-3"
         description="Uw email wordt tevens uw gebruikersnaam."
       ><b-form-input
           id="input-3"
@@ -45,8 +49,10 @@
         <input type="text" required v-model="email" />
       </div> -->
 
-      <b-form-group label="Geslacht">
-        <b-form-radio v-model="gender" name="gender" value="male">Man</b-form-radio>
+      <b-form-group
+        id="input-group-4"
+        label="Geslacht"
+      ><b-form-radio v-model="gender" name="gender" value="male">Man</b-form-radio>
         <b-form-radio v-model="gender" name="gender" value="female">Vrouw</b-form-radio>
       </b-form-group>
 
@@ -75,20 +81,41 @@
         </div>
       </div>
 
-      <div>
-        <h2>Adres (straatnaam en huisnummer)</h2>
-        <input type="text" required v-model="adress" />
-      </div>
+      <b-card bg-variant="light">
+        <b-form-group
+          label-cols-lg="3"
+          label="Adres"
+          label-size="lg"
+          label-class="font-weight-bold pt-0"
+          class="mb-0"
+        >
+          <b-form-group
+            label-cols-sm="3"
+            label="Adres:"
+            label-align-sm="right"
+            label-for="nested-street"
+          >
+            <b-form-input id="nested-street" required v-model="adress" placeholder= "Straatnaam en huisnummer"></b-form-input>
+          </b-form-group>
 
-      <div>
-        <h2>Postcode</h2>
-        <input type="text" required v-model="zipCode" />
-      </div>
-
-      <div>
-        <h2>Woonplaats</h2>
-        <input type="text" required v-model="city" />
-      </div>
+          <b-form-group
+            label-cols-sm="3"
+            label="Postcode:"
+            label-align-sm="right"
+            label-for="nested-city"
+          >
+            <b-form-input id="nested-city" required v-model="zipCode" placeholder="Bijv. 1012 AZ"></b-form-input>
+          </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label="Woonplaats:"
+            label-align-sm="right"
+            label-for="nested-city"
+          >
+            <b-form-input id="nested-city" required v-model="city" placeholder="Zaandam"></b-form-input>
+          </b-form-group>
+        </b-form-group>
+      </b-card>
 
       <div>
         <h2>Telefoonnummer</h2>
