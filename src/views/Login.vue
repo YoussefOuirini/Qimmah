968db0664c78b5790b1b10d67d2ee5e145522fc1
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-02 11:56:23 +0200

    creating login view

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container class="login">
+    <h3> Sign In</h3>
+    <b-input type="text" placeholder="Email"></b-input>
+    <b-input type="password" placeholder="Wachtwoord"></b-input>
+    <b-button>Connection</b-button>
+    <p>Nog geen account? Maak er een!</p>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {};
+  },
+  methods: {}
+}
+</script>
