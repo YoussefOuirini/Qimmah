ca2d6a0c529df3b8e84e9f73b70b106971b6a7b1
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-02 15:20:22 +0200

    putting router view in b-container to center it

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
-    <router-view></router-view>
+    <b-container>
+      <router-view></router-view>
+    </b-container>
     <Footer />
   </div>
 </template>
