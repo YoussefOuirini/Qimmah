7ecdeab956be7fcfe671c621bc96125c13f2f4a2
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-02 13:07:08 +0200

    Adding breaks

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <b-container class="login">
     <h3> Inloggen</h3>
-    <b-input type="text" placeholder="Email"></b-input>
-    <b-input type="password" placeholder="Wachtwoord"></b-input>
+    <b-input type="text" placeholder="Email"></b-input><br>
+    <b-input type="password" placeholder="Wachtwoord"></b-input><br>
     <b-button>Connection</b-button>
     <p>Nog geen account? Maak er een!</p>
   </b-container>
