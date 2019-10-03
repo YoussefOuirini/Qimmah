d3d13ee87ba61ced9b4dc1371ce9037a2af8d232
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-03 17:05:24 +0200

    Adding registrations component

---
+++ src/components/Registrations/Registrations.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+  import Vue from "vue";
+
+  export default Vue.extend ({
+    name: "Registrations"
+  })
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <b-container>
+    <Registrations />
     <br><b-button v-b-toggle.collapse-registration variant="primary"> Schrijf u of uw kind in!</b-button><br>
     <b-collapse id="collapse-registration" class="mt-2">
       <RegistrationForm />
@@ -13,11 +14,13 @@
 
   import Vue from "vue";
   import RegistrationForm from "../components/RegistrationForm/RegistrationForm.vue";
+  import Registrations from "../components/Registrations/Registrations.vue"
 
   export default Vue.extend({
     name: "App",
     components: {
       RegistrationForm,
+      Registrations
     },
     methods: {
       logout() {
