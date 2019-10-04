b13f0d7087a2d228f426df9e61d6eb2a177ca955
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-04 09:59:36 +0200

    loading data into container

--- src/components/Registrations/Registrations.vue
+++ src/components/Registrations/Registrations.vue
@@ -1,16 +1,31 @@
 <template>
-  <b-container>
-
+  <b-container v-if="isLoaded === true">
+    <b-row v-for="registration in registrations" v-bind:key="registration.id">
+      {{registration}}
+    </b-row>
   </b-container>
 </template>
 
 <script>
   import Vue from "vue";
+  import { getRegistrations } from "@/firebase";
 
   export default Vue.extend ({
     name: "Registrations",
+    mounted() {
+      this.loadRegistrations()
+    },
+    data() {
+      return {
+        isLoaded: false,
+        registrations: []
+      }
+    },
     methods: {
-      
+      async loadRegistrations() {
+        this.registrations = await getRegistrations()
+        this.isLoaded = true;
+      }
     }
   })
 </script>
