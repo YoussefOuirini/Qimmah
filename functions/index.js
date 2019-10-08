579a328479364f87c53a303b246efb6d5b3d836a
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-08 11:03:58 +0200

    backend function to get all users with https call

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.getAllUsers = functions.https.onCall((data) => {
+  return getUsers();
+});
+
+async function getUsers() {
+  const usersResult = await admin.auth().listUsers();
+  return usersResult.users;
+}
