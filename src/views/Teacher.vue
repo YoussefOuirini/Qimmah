c1940be06eff7d75c358c673919325324a52467c
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-08 11:03:16 +0200

    getting users and showing their data in a table

--- src/views/Teacher.vue
+++ src/views/Teacher.vue
@@ -1,13 +1,36 @@
 <template>
   <b-container>
-    <h1>Teacher</h1>
+    <b-table striped hover :items="users" :fields="fields"></b-table>
   </b-container>
 </template>
 
 <script>
 import Vue from 'vue';
+import firebase from 'firebase';
 
 export default Vue.extend({
-  name: "Teacher"
+  name: "Teacher",
+  mounted() {
+    this.searchUsers()
+  },
+  data() {
+    return {
+      users: [],
+      fields: [{
+          key: 'email',
+          label: 'Email'
+        },{
+          key: 'customClaims',
+          label: 'Autoriteit'
+        }],
+    }
+  },
+  methods: {
+    async searchUsers() {
+      const getAllUsers = firebase.functions().httpsCallable('getAllUsers');
+      const allUsers = await getAllUsers();
+      this.users = allUsers.data;
+    }
+  }
 })
 </script>
