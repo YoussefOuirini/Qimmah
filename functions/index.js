14486b598178e3f800c09569df26915f451f4d9c
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-08 14:41:04 +0200

    Adding backend add moderator logic and adding moderator role as security for getting users and changing roles

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,26 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.getAllUsers = functions.https.onCall((data) => {
-  return getUsers();
+exports.getAllUsers = functions.https.onCall((data, context) => {
+  if (context.auth.token.moderator) {
+    return getUsers();
+  }
+  return {error: 'User not authorized to view all users.'}
 });
 
 async function getUsers() {
   const usersResult = await admin.auth().listUsers();
   return usersResult.users;
+}
+
+exports.addModerator = functions.https.onCall((data, context) => {
+   if (context.auth.token.moderator) {
+     return setUserRole(data.email)
+   }
+   return { error: 'User not authorized to change user role.' }
+})
+
+async function setUserRole(email) {
+  const user = await admin.auth().getUserByEmail(email);
+  return admin.auth().setCustomUserClaims(user.uid, {moderator: true});
 }
