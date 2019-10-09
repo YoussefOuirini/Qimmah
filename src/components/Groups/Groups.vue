4e8ea5e350589312f9ac2c40f1c02b0ef701e041
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-09 10:37:16 +0200

    Adding empty Groups component

---
+++ src/components/Groups/Groups.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container></b-container>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default new Vue({
+  name: "Groups",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+})
+</script>
