c5c5d57d1df93d00e78e65eeeba7c924f119cc04
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-14 15:45:49 +0200

    Adding question if student made homework and put each student in his own StudentLesson component

--- src/components/Lesson/Lesson.vue
+++ src/components/Lesson/Lesson.vue
@@ -7,30 +7,12 @@
           <b-th>Naam</b-th>
           <b-th>Gedrag</b-th>
           <b-th>Aanwezigheid</b-th>
+          <b-th>Huiswerk gemaakt?</b-th>
           <b-th>Huiswerk</b-th>
         </b-tr>
       </b-thead>
       <b-tbody>
-        <b-tr v-for="student in students" v-bind:key="student.phoneNumber">
-          <b-td>{{student.name.first}} {{student.name.last}}</b-td>
-          <b-td>
-            <b-form-select v-model="behaviour">
-              <option value="good">Goed</option>
-              <option value="bad">Slecht</option>
-            </b-form-select>
-          </b-td>
-          <b-td>
-            <b-form-select v-model="presence">
-              <option value="present">Aanwezig</option>
-              <option value="late">Laat</option>
-              <option value="absent">Afwezig</option>
-            </b-form-select>
-          </b-td>
-          <b-td>
-            <b-form-textarea v-model='studentHomework' placeholder="Schrijf het huiswerk van deze student op"></b-form-textarea>
-          </b-td>
-        </b-tr>
-        <b-tr></b-tr>
+        <StudentLesson v-for="student in students" v-bind:key="student.id" v-bind:student="student" />
       </b-tbody>
       <b-tfoot></b-tfoot>
     </b-table-simple>
@@ -40,14 +22,19 @@
 <script>
 import Vue from "vue";
 import { writeLessons } from "../../firebase";
+import StudentLesson from "../StudentLesson/StudentLesson.vue"
 
 export default Vue.extend({
   name: "Lesson",
+  components: {
+    StudentLesson
+  },
   props: ["students"],
   data() {
     return {
-      behaviour: '',
-      presence: '',
+      behaviour: 'good',
+      presence: 'present',
+      madeHomework: true,
       studentHomework: ''
     }
   },
