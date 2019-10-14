d7c51147d744d48b73f78c6714f268be88d27635
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-14 15:31:49 +0200

    Adding body for student row

--- src/components/Lesson/Lesson.vue
+++ src/components/Lesson/Lesson.vue
@@ -10,6 +10,29 @@
           <b-th>Huiswerk</b-th>
         </b-tr>
       </b-thead>
+      <b-tbody>
+        <b-tr v-for="student in students" v-bind:key="student.phoneNumber">
+          <b-td>{{student.name.first}} {{student.name.last}}</b-td>
+          <b-td>
+            <b-form-select v-model="behaviour">
+              <option value="good">Goed</option>
+              <option value="bad">Slecht</option>
+            </b-form-select>
+          </b-td>
+          <b-td>
+            <b-form-select v-model="presence">
+              <option value="present">Aanwezig</option>
+              <option value="late">Laat</option>
+              <option value="absent">Afwezig</option>
+            </b-form-select>
+          </b-td>
+          <b-td>
+            <b-form-textarea v-model='studentHomework' placeholder="Schrijf het huiswerk van deze student op"></b-form-textarea>
+          </b-td>
+        </b-tr>
+        <b-tr></b-tr>
+      </b-tbody>
+      <b-tfoot></b-tfoot>
     </b-table-simple>
   </b-container>
 </template>
@@ -21,6 +44,13 @@ import { writeLessons } from "../../firebase";
 export default Vue.extend({
   name: "Lesson",
   props: ["students"],
+  data() {
+    return {
+      behaviour: '',
+      presence: '',
+      studentHomework: ''
+    }
+  },
   methods: {
     addLessons() {
       writeLessons()
