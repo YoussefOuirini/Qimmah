706096b8094cbde3468b5173b6858020aca7e39a
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-15 11:28:35 +0200

    Creating attendance page view page

---
+++ src/views/Attendance.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container>
+
+  </b-container>
+</template>
+
+<script>
+  import Vue from "vue";
+  import { getLessons } from "../firebase.js"
+
+  export default Vue.extend({
+    name: "Attendance",
+    async mounted() {
+      await this.loadStudents()
+    },
+    data() {
+      return {
+        students: []
+      }
+    },
+    methods: {
+      async loadStudents() {
+        this.students = await getLessons();
+      }
+    }
+  })
+</script>
