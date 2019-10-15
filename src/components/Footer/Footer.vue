7cca1b58838af5988067b8cdfac0091d65ae5eef
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-15 16:22:36 +0200

    Putting loggin out button in Footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -3,14 +3,37 @@
     <br>
     <b-container>
       &copy; 2019 Copyright: Youssef's creations
+      <br><b-button v-if="loggedInUser" @click="logout">Uitloggen</b-button>
     </b-container>
   </footer>
 </template>
 
 <script>
   import Vue from "vue";
+  import firebase from 'firebase';
+  import { EventBus } from "../../EventBus";
 
   export default Vue.extend({
-    name: "Footer"
+    name: "Footer",
+    mounted() {
+      this.loggedInUser = firebase.auth().currentUser;
+      EventBus.$on('userLoginChange', () => {
+        this.loggedInUser = firebase.auth().currentUser;
+      })
+    },
+    data() {
+      return {
+        loggedInUser: false
+      }
+    },
+    methods: {
+      logout() {
+        firebase.auth().signOut().then(() => {
+          EventBus.reloadLoggedinUser();
+          this.loggedInUser = firebase.auth().currentUser;
+          this.$router.replace('login')
+        })
+      }
+    }
   })
 </script>
