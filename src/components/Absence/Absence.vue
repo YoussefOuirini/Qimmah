2a29627d75774aa66ee6af880b03714fcf4cb650
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-16 12:22:05 +0200

    Adding selection for absence

--- src/components/Absence/Absence.vue
+++ src/components/Absence/Absence.vue
@@ -1,7 +1,21 @@
 <template>
-  <b-container v-if="registrations.length">
-    <h1>Meld uw kind af voor de les.</h1>
-  </b-container>
+  <b-form inline v-if="registrations.length">
+    <label class="mr-sm-2" for="inline-form-custom-select-pref">Meld uw kind af voor de les.</label>
+    <b-form-select
+      class="mb-2 mr-sm-2 mb-sm-0"
+      v-model="selectedRegistration"
+      :options="absenceRegistrations"
+      text-field="absenceInfo"
+    ></b-form-select>
+    <b-form-select
+      class="mb-2 mr-sm-2 mb-sm-0"
+      v-model="reasonOfAbsence"
+    >
+      <option value="ziek">Ziek</option>
+      <option value="overig">Overig</option>
+    </b-form-select>
+    <b-button variant="primary">Afmelden</b-button>
+  </b-form>
 </template>
 
 <script>
@@ -10,8 +24,20 @@
   export default Vue.extend({
     name: "Absence",
     props: ["registrations"],
-    mounted() {
-      
+    data() {
+      return {
+        selectedRegistration: "",
+        reasonOfAbsence: ""
+      }
     },
+    computed: {
+      absenceRegistrations() {
+        return this.registrations.map((registration) => {
+          const absenceRegistration = registration;
+          absenceRegistration.absenceInfo = `${registration.name.first} ${registration.name.last} ${registration.education} ${registration.group}`
+          return absenceRegistration;
+        })
+      }
+    }
   })
 </script>
