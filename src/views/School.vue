9571492938e73d25a1537070e9768358623d7fe1
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-18 10:53:13 +0200

    Adding new Absentees component to Schools

--- src/views/School.vue
+++ src/views/School.vue
@@ -2,21 +2,24 @@
   <b-container>
     <Groups v-if="users.length" v-bind:users="users"/>
     <Teachers v-if="users.length" v-bind:users="users"/>
+    <Absentees />
   </b-container>
 </template>
 
 <script>
 import Vue from 'vue';
 import firebase from 'firebase';
-import Teachers from '../components/Teachers/Teachers.vue'
-import Groups from '../components/Groups/Groups.vue'
-import { EventBus } from "../EventBus"
+import Teachers from '../components/Teachers/Teachers.vue';
+import Groups from '../components/Groups/Groups.vue';
+import Absentees from '../components/Absentees/Absentees.vue';
+import { EventBus } from "../EventBus";
 
 export default Vue.extend({
   name: "School",
   components: {
     Teachers,
-    Groups
+    Groups,
+    Absentees
   },
   mounted() {
     this.getUsers();
