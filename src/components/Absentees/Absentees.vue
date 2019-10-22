3de776bad5c242da35b7b190c16872fc31a49a9a
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-22 11:24:56 +0200

    Basic table to show absentees

--- src/components/Absentees/Absentees.vue
+++ src/components/Absentees/Absentees.vue
@@ -1,13 +1,20 @@
 <template>
   <b-container v-if="absentees.length">
     <h1>Bekijk de afwezigheidsmeldingen</h1>
-    {{absentees}}
+    <b-table
+      striped hover
+      :items="absentees"
+      :fields="absenteeFields"
+    >
+      
+    </b-table>
   </b-container>
 </template>
 
 <script>
 import Vue from 'vue';
 import {getAllAbsentees} from "../../firebase";
+import { getAge } from "../../common/getAge.js";
 
 export default Vue.extend({
   name: "Absentees",
@@ -16,12 +23,38 @@ export default Vue.extend({
   },
   data() {
     return {
-      absentees: []
+      absentees: [],
+      absenteeFields: [{
+        key: "name",
+        label: "Naam",
+        formatter: (value) => {
+          return `${value.first} ${value.last}`
+        }
+      }, {
+        key: "education",
+        label: "Onderwijs"
+      }, {
+        key: "group",
+        label: "Klas"
+      }, {
+        key: "birthDate",
+        label: "Leeftijd",
+        formatter: (value) => {
+          return getAge(value)
+        },
+      }, {
+        key: 'phoneNumber',
+        label: 'Telefoonnummer'
+      }, {
+        key: 'group',
+        label: 'Klas'
+      }],
     }
   },
   methods: {
     async loadAbsentees() {
       this.absentees = await getAllAbsentees();
+      // console.log(this.absentees);
     }
   }
 })
