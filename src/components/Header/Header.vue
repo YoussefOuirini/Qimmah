6b95b376b6f830b60baf9dcbb7ed96bfef0f39c6
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-23 15:43:49 +0200

    Adding own background image and Logo

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,11 +3,11 @@
     <header id="masthead" class="site-header" role="banner">
       <div class="custom-header">
         <div class="custom-header-media">
-          <div id="wp-custom-header" class="wp-custom-header"><img src="https://www.alhimmah.nl/wp-content/uploads/2018/05/cropped-Zonsondergang-in-Zaandam.jpg" width="2000" height="1198" alt="Stichting Al-Himmah" srcset="https://www.alhimmah.nl/wp-content/uploads/2018/05/cropped-Zonsondergang-in-Zaandam.jpg 2000w, https://www.alhimmah.nl/wp-content/uploads/2018/05/cropped-Zonsondergang-in-Zaandam-300x180.jpg 300w, https://www.alhimmah.nl/wp-content/uploads/2018/05/cropped-Zonsondergang-in-Zaandam-768x460.jpg 768w, https://www.alhimmah.nl/wp-content/uploads/2018/05/cropped-Zonsondergang-in-Zaandam-1024x613.jpg 1024w" sizes="100vw" /></div>
+          <div id="wp-custom-header" class="wp-custom-header"><img src="../../assets/qimmah.jpg" width="2000" height="1198" alt="Qimmah" sizes="100vw" /></div>
         </div>
         <div class="site-branding">
             <div class="wrap">
-              <a href="/" class="custom-logo-link" rel="home"><img width="253" height="250" src="https://www.alhimmah.nl/wp-content/uploads/2018/06/cropped-logo-circle.png" class="custom-logo" alt="Stichting Al-Himmah" srcset="https://www.alhimmah.nl/wp-content/uploads/2018/06/cropped-logo-circle.png 253w, https://www.alhimmah.nl/wp-content/uploads/2018/06/cropped-logo-circle-100x100.png 100w" sizes="100vw" /></a>
+              <a href="/" class="custom-logo-link" rel="home"><img width="253" height="250" src="../../assets/qimmah-background.jpg" class="custom-logo" alt="Qimmah" sizes="100vw" /></a>
               <div class="site-branding-text">
                 <p class="site-title"><a href="/" rel="home">Qimmah</a></p>
               </div><!-- .site-branding-text -->
