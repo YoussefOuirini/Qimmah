1ab9309bb2a026ea1db5e3f2064f7bfd525eb9fa
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-10-24 11:56:30 +0200

    Adding sendgrid email sending to functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
+const sendgridMail = require("@sendgrid/mail");
+sendgridMail.setApiKey(functions.config().sendgrid.apikey);
+
 admin.initializeApp();
 
 exports.getAllUsers = functions.https.onCall((data, context) => {
@@ -10,17 +12,32 @@ exports.getAllUsers = functions.https.onCall((data, context) => {
   return {error: 'User not authorized to view all users.'}
 });
 
-async function getUsers() {
-  const usersResult = await admin.auth().listUsers();
-  return usersResult.users;
-}
-
 exports.addModerator = functions.https.onCall((data, context) => {
    if (context.auth.token.moderator) {
      return setUserRole(data.email, data.role)
    }
    return { error: 'User not authorized to change user role.'}
 })
+
+exports.sendEmail = functions.https.onCall((data, context) => {
+  if (context.auth.token.teacher || context.auth.token.moderator) {
+    const { toEmail, subject, text, html } = data;
+    const msg = {
+      to: toEmail,
+      from: 'qimmah@himmah.com',
+      subject: subject,
+      text: text,
+      html: html,
+    };
+    return sendgridMail.send(msg);
+  }
+  return { error: 'User not authorized to change user role.'}
+})
+
+async function getUsers() {
+  const usersResult = await admin.auth().listUsers();
+  return usersResult.users;
+}
 
 async function setUserRole(email, role) {
   const user = await admin.auth().getUserByEmail(email);
