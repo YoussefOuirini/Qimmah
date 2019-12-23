69267a786eae8a094d2490f4af2c62974d119382
Youssef Ouirini <youssefouirini@gmail.com>
2019-12-23 10:50:40 +0100

    removing extra container wrap

--- src/components/Groups/Groups.vue
+++ src/components/Groups/Groups.vue
@@ -1,23 +1,21 @@
 <template>
   <b-row>
-    <b-container>
-      <b-form-group
-        id="group"
-        description="De naam zal worden toegevoegd aan de klassen."
-        label="Creëer een nieuwe klas."
-        label-for="group"
-        :invalid-feedback="invalidFeedback"
-        :valid-feedback="validFeedback"
-        :state="state"
-      >
-        <b-input-group class="mt-3">
-          <b-form-input id="group" v-model="groupName" :state="state" trim></b-form-input>
-          <b-input-group-append>
-            <b-button @click="addGroup" variant="primary" :disabled="groupAlreadyExists">Klas toevoegen</b-button>
-          </b-input-group-append>
-        </b-input-group>
-      </b-form-group>
-    </b-container>
+    <b-form-group
+      id="group"
+      description="De naam zal worden toegevoegd aan de klassen."
+      label="Creëer een nieuwe klas."
+      label-for="group"
+      :invalid-feedback="invalidFeedback"
+      :valid-feedback="validFeedback"
+      :state="state"
+    >
+      <b-input-group class="mt-3">
+        <b-form-input id="group" v-model="groupName" :state="state" trim></b-form-input>
+        <b-input-group-append>
+          <b-button @click="addGroup" variant="primary" :disabled="groupAlreadyExists">Klas toevoegen</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-form-group>
     <Teachers v-if="groups.length && users.length" v-bind:groups="groups" v-bind:users="users" @reloadGroups="loadGroups"/>
     <Students v-if="registrations.length" v-bind:registrations="registrations" v-bind:groups="groups" @reloadRegistrations="loadRegistrations"/>
   </b-row>
