27ee0bc2f0efec5484ecb4d0d908679c16ac83ac
Youssef Ouirini <youssefouirini@gmail.com>
2020-01-29 16:20:17 +0100

    removing b-table-simple components and StudentGroup component

--- src/components/Groups/Students/Students.vue
+++ src/components/Groups/Students/Students.vue
@@ -19,37 +19,17 @@
             <b-button @click="addRegistrationToGroup(registration)" size="sm"> Toevoegen aan klas</b-button>
           </b-form>
         </template>
-        <b-thead>
-          <b-tr>
-            <b-th>Naam</b-th>
-            <b-th>Onderwijs</b-th>
-            <b-th>Leeftijd</b-th>
-            <b-th>Klas</b-th>
-          </b-tr>
-        </b-thead>
-        <b-tbody>
-          <b-tr v-for="registration in registrations" v-bind:key="registration.id">
-            <b-td> {{registration.name.first}} {{registration.name.last}}</b-td>
-            <b-td >{{registration.education}}</b-td>
-            <b-td>{{calculatedAge(registration.birthDate)}}</b-td>
-            <StudentGroup :registration="registration" :groups="groups" @reloadRegistration="reloadRegistrations"/>
-          </b-tr>
-        </b-tbody>
       </b-table>
   </b-row>
 </template>
 
 <script>
 import Vue from 'vue';
-import StudentGroup from './StudentGroup.vue';
 import { getAge } from "@/common/getAge.js";
 import { writeStudentToGroup, updateRegistration, removeStudentFromGroups } from "@/firebase/firebase.js";
 
 export default Vue.extend({
   name: "Students",
-  components: {
-    StudentGroup
-  },
   props: ['registrations', 'groups'],
   data() {
     return {
