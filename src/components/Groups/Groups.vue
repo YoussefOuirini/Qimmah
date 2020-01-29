f86cef7419ddf5ba8dd626a77b1ca454ad358e68
Youssef Ouirini <youssefouirini@gmail.com>
2020-01-29 12:55:09 +0100

    removing additional row and centering form-row

--- src/components/Groups/Groups.vue
+++ src/components/Groups/Groups.vue
@@ -1,25 +1,23 @@
 <template>
   <b-row>
     <Teachers v-if="groups.length && users.length" v-bind:groups="groups" v-bind:users="users" @reloadGroups="loadGroups"/>
-    <b-row v-if="groups.length && users.length" class="my-1">
-      <b-form-row>
-        <b-form-group
-          id="group"
-          label="Creëer een nieuwe klas."
-          label-for="group"
-          :invalid-feedback="invalidFeedback"
-          :valid-feedback="validFeedback"
-          :state="state"
-        >
-          <b-input-group class="mt-3">
-            <b-form-input id="group" v-model="groupName" :state="state" trim></b-form-input>
-            <b-input-group-append>
-              <b-button @click="addGroup" variant="success" :disabled="groupAlreadyExists">Klas toevoegen</b-button>
-            </b-input-group-append>
-          </b-input-group>
-        </b-form-group>
-      </b-form-row>
-    </b-row>
+    <b-form-row v-if="groups.length && users.length" class="mx-5">
+      <b-form-group
+        id="group"
+        label="Creëer een nieuwe klas."
+        label-for="group"
+        :invalid-feedback="invalidFeedback"
+        :valid-feedback="validFeedback"
+        :state="state"
+      >
+        <b-input-group class="mt-3">
+          <b-form-input id="group" v-model="groupName" :state="state" trim></b-form-input>
+          <b-input-group-append>
+            <b-button @click="addGroup" variant="success" :disabled="groupAlreadyExists">Klas toevoegen</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form-group>
+    </b-form-row>
     <Students v-if="registrations.length" v-bind:registrations="registrations" v-bind:groups="groups" @reloadRegistrations="loadRegistrations"/>
   </b-row>
 </template>
