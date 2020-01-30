82a9e67772640199197b50a059959e846093c059
Youssef Ouirini <youssefouirini@gmail.com>
2020-01-30 16:24:35 +0100

    adding options to get 2-digit dates for months and day

--- src/common/getDate.js
+++ src/common/getDate.js
@@ -1,10 +1,11 @@
 export function getLessonDate(timeStamp) {
+  const options = { day:'2-digit', month:'2-digit', year: 'numeric'};
   if (timeStamp) {
     const lessonDate = new Date(timeStamp);
-    return lessonDate.toLocaleDateString("nl-NL");
+    return lessonDate.toLocaleDateString("nl-NL", options);
   }
   const lessonDate = new Date();
-  return lessonDate.toLocaleDateString("nl-NL");
+  return lessonDate.toLocaleDateString("nl-NL", options);
 }
 
 export function getTimeStamp(dateString) {
