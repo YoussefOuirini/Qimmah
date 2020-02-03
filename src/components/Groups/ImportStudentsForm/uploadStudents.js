74bb8a2f4ac2a48c169fff845b9cf72ea84d7cc9
Youssef Ouirini <youssefouirini@gmail.com>
2020-02-03 12:03:07 +0100

    separating mapping and iterating over the students

--- src/components/Groups/ImportStudentsForm/uploadStudents.js
+++ src/components/Groups/ImportStudentsForm/uploadStudents.js
@@ -4,54 +4,59 @@ export function uploadStudents(file) {
   const reader = new FileReader();
   reader.onload(() => {
     csv.parse((reader.result, (err, data) => {
-      mapStudentsJSON(data);
+      getStudentsJSON(data);
     }));
   });
   reader.readAsBinaryString(file);
 }
 
-function mapStudentsJSON(data) {
+function getStudentsJSON(data) {
   let studentsList = [];
       
   for (let i = 0; i < data.length; i++) {
-    const firstName = data[i][0];
-    const lastName = data[i][1];
-    const gender = data[i][2];
-    const birthDate = data[i][3];
-    const email = data[i][4];
-    const zipCode = data[i][5];
-    const city = data[i][6];
-    const streetName = data[i][7];
-    const education = data[i][8];
-    const parentFirstName = data[i][9];
-    const parentLastName = data[i][10];
-    const phoneNumber = data[i][11];
-
-    const newStudent = {
-      'name': {
-        first: firstName,
-        last: lastName
-      },
-      gender,
-      birthDate,
-      email,
-      address: {
-        zipCode,
-        city,
-        streetName
-      },
-      education,
-      parent: {
-        name: {
-          first: parentFirstName,
-          last: parentLastName
-        }
-      },
-      phoneNumber
-    };
-
+    const newStudent = mapStudentJSON(data, i);
     studentsList.push(newStudent);
   }
 
   return studentsList;
+}
+
+function mapStudentJSON(data, i) {
+  const firstName = data[i][0];
+  const lastName = data[i][1];
+  const gender = data[i][2];
+  const birthDate = data[i][3];
+  const email = data[i][4];
+  const zipCode = data[i][5];
+  const city = data[i][6];
+  const streetName = data[i][7];
+  const education = data[i][8];
+  const parentFirstName = data[i][9];
+  const parentLastName = data[i][10];
+  const phoneNumber = data[i][11];
+
+  const newStudent = {
+    name: {
+      first: firstName,
+      last: lastName
+    },
+    gender,
+    birthDate,
+    email,
+    address: {
+      zipCode,
+      city,
+      streetName
+    },
+    education,
+    parent: {
+      name: {
+        first: parentFirstName,
+        last: parentLastName
+      }
+    },
+    phoneNumber
+  };
+
+  return newStudent;
 }
