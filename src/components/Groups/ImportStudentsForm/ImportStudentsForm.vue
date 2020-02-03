473b211f5afcd4d3e7d31a07bd8d9b8610c6ab47
Youssef Ouirini <youssefouirini@gmail.com>
2020-02-03 10:20:26 +0100

    only accepting csv files

--- src/components/Groups/ImportStudentsForm/ImportStudentsForm.vue
+++ src/components/Groups/ImportStudentsForm/ImportStudentsForm.vue
@@ -8,11 +8,12 @@
           placeholder="Selecteer een CSV document of sleep het hier naar toe..."
           drop-placeholder="Drop file here..."
           class="m-5 px-5"
+          accept=".csv"
         ></b-form-file>
       </b-col>
     </b-form-row>
-    <b-form-row class="mt-3">
-      <b-col class="m-5 px-5">Geselecteerde document: {{ file ? file.name : '' }}</b-col>
+    <b-form-row>
+      <b-col class="m-5 px-4">Geselecteerde document: {{ file ? file.name : '' }}</b-col>
     </b-form-row>
   </b-row>
 </template>
