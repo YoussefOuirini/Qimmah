5acc1cd76152c69aadf5162a03ed50058aa83b60
Youssef Ouirini <youssefouirini@gmail.com>
2020-02-03 10:17:28 +0100

    Adding import file template

--- src/components/Groups/ImportStudentsForm/ImportStudentsForm.vue
+++ src/components/Groups/ImportStudentsForm/ImportStudentsForm.vue
@@ -1,11 +1,30 @@
 <template>
-  <b-row></b-row>
+  <b-row>
+    <b-form-row>
+      <b-col>
+        <b-form-file
+          v-model="file"
+          :state="Boolean(file)"
+          placeholder="Selecteer een CSV document of sleep het hier naar toe..."
+          drop-placeholder="Drop file here..."
+          class="m-5 px-5"
+        ></b-form-file>
+      </b-col>
+    </b-form-row>
+    <b-form-row class="mt-3">
+      <b-col class="m-5 px-5">Geselecteerde document: {{ file ? file.name : '' }}</b-col>
+    </b-form-row>
+  </b-row>
 </template>
 
 <script>
 import Vue from 'vue';
 
 export default Vue.extend({
-  
+  data() {
+    return {
+      file: null
+    }
+  }
 })
 </script>
