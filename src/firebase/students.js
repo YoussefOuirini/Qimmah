b61391930476f8cc4127a67066b6efcd2ed30ccf
Youssef Ouirini <youssefouirini@gmail.com>
2020-02-11 14:55:21 +0100

    removing moderator check so users can remove their registrations from groups

--- src/firebase/students.js
+++ src/firebase/students.js
@@ -9,10 +9,6 @@ export async function deleteStudent(student) {
 }
 
 export async function removeStudentFromGroups(student, groups) {
-  const userIsModerator = await checkUserClaim('moderator');
-  if (!userIsModerator) {
-    return new Error('User not authorized.');
-  }
   const studentDocName = getStudentDocName(student);
   groups.forEach((group) => {
     db.collection('groups').doc(group.groupName).collection('students').doc(studentDocName).delete()
