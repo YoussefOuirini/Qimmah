ff9fb3e660a5183856c95d69d338b818c0bd10e0
Youssef Ouirini <youssefouirini@gmail.com>
2020-03-13 12:15:05 +0100

    removing unused providers

--- src/firebase/firebaseui.js
+++ src/firebase/firebaseui.js
@@ -1,19 +1,14 @@
 import firebase from "firebase/app";
-import firebaseui from 'firebaseui';
+import * as firebaseui from 'firebaseui';
 import { auth } from './firebase';
 
-const uiConfig = {
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+export const uiConfig = {
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.MicrosoftAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
   ]
 };
 
 // Initialize the FirebaseUI Widget using Firebase.
 export const ui = new firebaseui.auth.AuthUI(auth);
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
