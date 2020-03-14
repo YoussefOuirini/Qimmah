5bc9d03498376a9f7f18f4c53e5f4fd4d07542ac
Youssef Ouirini <youssefouirini@gmail.com>
2020-03-14 09:22:45 +0100

    redirecting to home when successfully signed in

--- src/firebase/firebaseui.js
+++ src/firebase/firebaseui.js
@@ -3,6 +3,7 @@ import * as firebaseui from 'firebaseui';
 import { auth } from './firebase';
 
 export const uiConfig = {
+  signInSuccessUrl: 'home',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
