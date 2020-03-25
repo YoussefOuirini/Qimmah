ac0847ed8ee25b7fa7755f18ec9adbb1d7c68827
Youssef Ouirini <youssefouirini@gmail.com>
2020-03-25 09:27:38 +0100

    creating a spearate jitsi vue component

---
+++ src/components/Jitsi/Jitsi.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id='jitsi'><div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { joinCall } from "../../jitsi";
+
+export default Vue.extend({
+  name: "Jitsi",
+  mounted() {
+    joinCall(this.groupName)
+  },
+  data() {
+    return {
+      groupName: ''
+    }
+  }
+})
+</script>
